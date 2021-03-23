4ad2563e3b6ff3fd3d459528d9b98eb778349008
ElinaValieva <veaufa@mail.ru>
2021-03-23 19:52:17 +0300

    Fix: test

--- caching/src/main/java/com/patterns/caching/model/User.java
+++ caching/src/main/java/com/patterns/caching/model/User.java
@@ -3,10 +3,12 @@ package com.patterns.caching.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User extends BaseModel {
 
     private String id;
